f5acd9c43b29283269462394e8eb167936ebd24a
Luck <pawat.rew@mail.kmutt.ac.th>
2021-09-15 21:57:32 +0700

    [ADD] Mysql and test connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/users.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'mysql',
+    database: 'witpakul_db',
+    entities: [UserEntity],
+    synchronize: true,
+  }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
+

131a9d3c5622d3e5c30e5732f049146953d1d5da
Earty4516 <worawit.4516@mail.kmutt.ac.th>
2021-10-04 13:36:02 +0700

    [FIX]Nestjs Core

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule , { cors: true });
   await app.listen(4000);
+  app.enableCors();
+  
 }
 bootstrap();
